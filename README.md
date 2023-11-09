90cec7fed68788a25e12edf6970dc31de860ca2d
myjourney23 <myjourneysites23@gmail.com>
2023-11-10 01:11:18 +0900

    first commit of Bob-resume

---
+++ README.md
@@ -0,0 +1,14 @@
+# Portfolio Website Project
+
+This is the source code for the template resume website. Clients can view this site to have an idea of how their resume will be displayed.
+
+## Built Using
+
+* HTML
+* CSS
+* JavaScript
+* [Roboto_Condensed](https://fonts.google.com/specimen/Roboto+Condensed?query=roboto+con)
+
+## Additional
+
+You can visit the website by going [here](https://jreyes73.github.io/template-resume/).
---
+++ index.js
@@ -0,0 +1,16 @@
+// alert("Welcome to my page.");    
+
+var coll = document.getElementsByClassName("drop-down-box");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "flex") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "flex";
+    }
+  });
+}
